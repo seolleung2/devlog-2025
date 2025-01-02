c79afc1aa94f029b1e0a1370c20efc34189d2464
seolleung2 <seolleung22@gmail.com>
2025-01-03 00:02:31 +0900

    install prettier, prettier-plugin-tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline border-red-500 border">
+      <h1 className="border border-solid border-red-500 text-3xl font-bold underline">
         Hello World
       </h1>
       <Button>Click me</Button>
