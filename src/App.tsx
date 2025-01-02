de5f26c12a4853473c1ed6044c76be95756b248e
seolleung2 <seolleung22@gmail.com>
2025-01-03 00:00:39 +0900

    install shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <h1 className="text-3xl font-bold underline border-red-500 border">
+        Hello World
+      </h1>
+      <Button>Click me</Button>
     </>
   );
 }
