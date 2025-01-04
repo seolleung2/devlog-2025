6d184b9b6f7e1b5dd957360d3c8789c4a5834fc1
seolleung2 <seolleung22@gmail.com>
2025-01-04 19:15:26 +0900

    add AuthContext, AuthProvider

---
+++ src/contexts/AuthProvider.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+import { auth } from "@/lib/firebase/firebase";
+import { AuthContext } from "./AuthContext";
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
