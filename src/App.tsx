daa4aa6c9d7412242dcf8dcf092fb5471bf4841c
seolleung2 <seolleung22@gmail.com>
2025-01-05 11:49:55 +0900

    update requirements.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { AuthProvider } from "@/contexts/AuthProvider";
+import AuthProvider from "@/contexts/AuthProvider";
 
 function App() {
   return (
