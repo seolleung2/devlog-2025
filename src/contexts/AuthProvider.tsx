65c5fc93776e1ad644429873fdafae69e500e5f8
seolleung2 <seolleung22@gmail.com>
2025-01-05 11:47:31 +0900

    setIsAdin whether the user is admin or not when user logged in

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react";
 import { User } from "firebase/auth";
 import { auth } from "@/lib/firebase/firebase";
+import { isAdminEmail } from "@/lib/firebase/admin";
 import { AuthContext } from "./AuthContext";
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
       setUser(user);
+      setIsAdmin(await isAdminEmail(user?.email));
       setLoading(false);
     });
 
@@ -17,7 +24,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, loading }}>
+    <AuthContext.Provider value={{ user, loading, isAdmin }}>
       {children}
     </AuthContext.Provider>
   );
