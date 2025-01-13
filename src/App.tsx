5956e7c39629b1edcf102fe93d090406a00cd4b6
seolleung2 <seolleung22@gmail.com>
2025-01-14 00:35:30 +0900

    wrapping App component with QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import AuthProvider from "@/contexts/AuthProvider";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, // 1분
+    },
+  },
+});
+
 function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </AuthProvider>
   );
 }
